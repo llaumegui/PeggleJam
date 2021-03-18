65c97f4589e03d721cf6892940ee97c24a5ddb4a
llaumegui <guillaumelef59@gmail.com>
2021-03-18 17:29:48 +0100

    Replace

---
+++ Peggle/Assets/Replace.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Replace : MonoBehaviour
+{
+	public GameObject O;
+	public List<GameObject> ObjToReplace;
+
+	[ContextMenu("ptn")]
+	public void Replacer()
+	{
+		for (int i = ObjToReplace.Count - 1; i >= 0; i--)
+		{
+			Instantiate(O, ObjToReplace[i].transform.position, Quaternion.identity);
+
+			DestroyImmediate(ObjToReplace[i]);
+		}
+	}
+}
